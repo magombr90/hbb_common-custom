f9cf2e74cb100b9d56d98517acafb7ba633ea189
magombr90 <mailton.gom@gmail.com>
2026-09-25 15:12:45 -0300

    Embed custom relay server and public key

--- src/config.rs
+++ src/config.rs
@@ -114,8 +114,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["72.61.44.57"];
+pub const RS_PUB_KEY: &str = "gYIZEpzwufEYVPRqe45AgsVjNHEfdiKfHJFDo8Rsyvc";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
